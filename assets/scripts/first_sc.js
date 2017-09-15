38efe2e0bff7fce7b482815214f6701d73bb2399
Mezernyi <mezernyi@gmail.com>
2017-09-16 02:19:40 +0300

    1

---
+++ assets/scripts/first_sc.js
@@ -0,0 +1,23 @@
+// $(document).ready(function() {
+// 	$('#loginForm').submit(function(){
+// 		$.post("http://codeit.pro/frontTestTask/user/registration", $("#loginForm").serialize(),  function(data) {
+// 			// $('#loginForm').hide('slow');
+// 			$('.form__answer').html(data);
+// 			});
+// 		return false;
+// 	});
+// });	
+
+function inf() {
+  var msg   = $('#lolinform').serialize();
+
+$.ajax({
+  type: "POST",
+  url: 'http://codeit.pro/frontTestTask/user/registration',
+  data: msg,
+  success: function (){
+  	$('.form__answer').html(data)
+  },
+  dataType: dataType
+});
+};
