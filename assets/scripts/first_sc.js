380521cd8fb91dc555b3354e370d464564f602f9
Mezernyi <mezernyi@gmail.com>
2017-09-16 02:33:23 +0300

    2

--- assets/scripts/first_sc.js
+++ assets/scripts/first_sc.js
@@ -1,23 +1,24 @@
-// $(document).ready(function() {
-// 	$('#loginForm').submit(function(){
-// 		$.post("http://codeit.pro/frontTestTask/user/registration", $("#loginForm").serialize(),  function(data) {
-// 			// $('#loginForm').hide('slow');
-// 			$('.form__answer').html(data);
-// 			});
-// 		return false;
-// 	});
-// });	
-
-function inf() {
-  var msg   = $('#lolinform').serialize();
-
-$.ajax({
-  type: "POST",
-  url: 'http://codeit.pro/frontTestTask/user/registration',
-  data: msg,
-  success: function (){
-  	$('.form__answer').html(data)
-  },
-  dataType: dataType
+"use strict";
+$(document).ready(function() {
+	$('#loginForm').submit(function(){
+	$.post("http://codeit.pro/frontTestTask/user/registration", $("#loginForm").serialize(),  function(data) {
+	// $('#loginForm').hide('slow');
+	$('.form__answer').html(data);
 });
-};
+		return false;
+	});
+});	
+
+// function inf() {
+//   var msg   = $('#loginform').serialize();
+
+// $.ajax({
+//   type: "POST",
+//   url: 'http://codeit.pro/frontTestTask/user/registration',
+//   data: msg,
+//   success: function (){
+//   $('.form__answer').html(data)
+//   },
+//   dataType: dataType
+// });
+// };
